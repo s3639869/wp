92dffdc88e432ba8f5f89e24d5c07829c515435b
s3639869 <s3639869@rmit.edu.vn>
2020-05-21 01:17:14 +0700

    improve receipt css, add receipt heading, fix price table, add total price and gst

--- a4/receipt.php
+++ a4/receipt.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Receipt</title>
+    <link id='stylecss' type="text/css" rel="stylesheet" href="receipt-style.css">
     <?php include 'tools.php';?>
 </head>
 <body>
@@ -11,8 +12,129 @@
     if(empty($_SESSION)){
         header("Location: index.php");
     }
-    echo "<h1>Welcome to the receipt page......</h1>";
-    preShow($_SESSION);
+
+    $now = date('d/m/yy h:i');
+    $bookingCells = array_merge(
+        [$now],
+        $_SESSION['cart']['cust'],
+        $_SESSION['cart']['movie'],
+        $_SESSION['cart']['seats'],
+        [calcTotalSession()]
+    );
+
+    $bookingSheet = fopen("bookings.txt", "a");
+    flock($bookingSheet, LOCK_EX);
+    fputcsv($bookingSheet, $bookingCells, "\t");
+    flock($bookingSheet, LOCK_UN);
+    fclose($bookingSheet);
     ?>
+
+    <div class="page">
+        <div id="invoice-header">
+        <div class="head-strip"></div>
+        <img src="media/cinemax_logo.png" alt="Cinemax logo" id="invoice-logo">
+        <p id="anb-num"><b>ABN number:</b> 00 123 456 789</p>
+        <div id="company-info">
+            <table id="header-table">
+                <tr>
+                    <td class="header-info">
+                        <img src="media/map.svg" alt="Map icon" class="header-icon">
+                        <br>
+                        <b>Address:</b><br>221B Baker Street Faraway City
+                    </td>
+                    <td class="header-info">
+                        <img src="media/envelope.svg" alt="Phone icon" class="header-icon">
+                        <br>
+                        <b>Email:</b><br>contact_support@cinemax.com
+                    </td>
+                    <td class="header-info">
+                        <img src="media/phone.svg" alt="Phone icon" class="header-icon">
+                        <br>
+                        <b>Phone number:</b><br>+84 900779977
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div class="head-strip"></div>
+        </div>
+        <br>
+        <h2 id="invoice-heading">INVOICE <span id="invoice-date">Date: <?php echo $now?></span></h2>
+        <table style='width:100%;' class="info-table">
+        <tr>
+            <th class="info-table cust-info">Customer Information</th>
+            <th class="info-table movie-info">Movie Information</th>
+        </tr>
+        <tr>
+            <td class="info-table cust-info">
+                <p><span class="info-title">Name:</span> <?php echo $_SESSION['cart']['cust']['name']?></p>
+                <p><span class="info-title">Email:</span> <?php echo $_SESSION['cart']['cust']['email']?></p>
+                <p><span class="info-title">Mobile:</span> <?php echo $_SESSION['cart']['cust']['mobile']?></p>
+            </td>
+            <td class="info-table movie-info">
+                <p><span class="info-title">Movie title:</span> <?php echo $movieID[$_SESSION['cart']['movie']['id']]?></p>
+                <p><span class="info-title">Movie day:</span> <?php echo $days[$_SESSION['cart']['movie']['day']]?></p>
+                <p><span class="info-title">Movie time:</span> <?php echo $timeConvert[$_SESSION['cart']['movie']['hour']]?></p>
+            </td>
+        </tr>
+        </table>
+        <h2 id="seat-heading">Order Information</h2>
+        <table style='width:100%;' class="seats-table">
+        <tr>
+            <th class="seats-table seats-head">Seat Description</th>
+            <th class="seats-table seats-head">Seat Code</th>
+            <th class="seats-table seats-head">Qty</th>
+            <th class="seats-table seats-head">Unit price</th>
+            <th class="seats-table seats-head">Subtotal</th>
+        </tr>
+        <?php
+            foreach($_SESSION['cart']['seats'] as $type => $qty){
+                if($qty > 0){
+                    echo "<tr><td class='seats-table' style='text-align: center;'>".$seatTypes[$type]."</td>";
+                    echo "<td class='seats-table' style='text-align: center;'>".$type."</td>";
+                    echo "<td class='seats-table' style='text-align: center;'>".$qty."</td>";
+                    if($_SESSION['cart']['movie']['day']=='MON' or $_SESSION['cart']['movie']['day']=='WED' or (in_array($_SESSION['cart']['movie']['day'], $weekDays) and $_SESSION['cart']['movie']['hour']=='T12')){
+                        echo "<td class='seats-table' style='text-align: center;'>$".number_format((float)$seatDiscount[$type], 2)."</td>";
+                        echo "<td class='seats-table' style='text-align: center;'>$".number_format((float)$seatDiscount[$type]*$qty, 2)."</td>";
+                    }
+                    else{
+                        echo "<td class='seats-table' style='text-align: center;'>$".number_format((float)$seatFull[$type], 2)."</td>";
+                        echo "<td class='seats-table' style='text-align: center;'>$".number_format((float)$seatFull[$type]*$qty, 2)."</td>";
+                    }
+                    echo "</tr>";
+                }
+            }
+        ?>
+        </table>
+        <table id="gst-total">
+            <tr>
+                <td class="gst-head">
+                    <b>Total Price: </b>
+                </td>
+                <td class="gst-calc">
+                    $<?php echo calcTotalSession(); ?>
+                </td>
+            </tr>
+            <tr style="border-bottom: teal 3px solid;">
+                <td class="gst-head">
+                    <b>GST: </b>
+                </td>
+                <td class="gst-calc">
+                    $<?php echo calcTotalSession()*0.1; ?>
+                </td>
+            </tr>
+            <tr>
+                <td class="gst-head">
+                    <b>Grand Total: </b>
+                </td>
+                <td class="gst-calc">
+                    $<?php echo calcTotalSession()*1.1; ?>
+                </td>
+            </tr>
+        </table>
+        <h2 id="ticket-heading">Print your tickets</h2>
+        <hr style="border: teal solid 2px;">
+        <h3 style="color: teal; text-align: right;">Thank you for your business!</h3>
+    </div>
+    <?php preShow($bookingCells); ?>
 </body>
 </html>
